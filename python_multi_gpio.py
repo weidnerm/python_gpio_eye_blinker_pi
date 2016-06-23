f1a5a8fa49f6abf7cf53f5ca4b4917fa8219266d
Michael Weidner <michael.weidner@motorolasolutions.com>
2016-06-23 00:48:16 -0400

    add independent worker threads.

--- python_multi_gpio.py
+++ python_multi_gpio.py
@@ -4,6 +4,9 @@ import thread
 
 
 brightness = 0.0
+gpioNumber = [2,3,4,17,27]
+
+RPi.GPIO.setmode(RPi.GPIO.BCM)
 
 def setBrightnessThread():
 	global brightness
@@ -44,58 +47,42 @@ def setBrightnessThread():
 
 
 
-def brightnessWorker():
+def brightnessWorker(name, gpioIndex):
 	global brightness
+	global gpioNumber
 
-	count = 0
-	RPi.GPIO.setmode(RPi.GPIO.BCM)
-	
-	# pin 11 is GPIO17
-	RPi.GPIO.setup(2, RPi.GPIO.OUT)
-	RPi.GPIO.setup(3, RPi.GPIO.OUT)
-	RPi.GPIO.setup(4, RPi.GPIO.OUT)
-	RPi.GPIO.setup(17, RPi.GPIO.OUT)
-	RPi.GPIO.setup(27, RPi.GPIO.OUT)
+	print "starting %s on gpioNumber[%d] = %d" % (name, gpioIndex, gpioNumber[gpioIndex] )
 
-	RPi.GPIO.output(2, False)
-	RPi.GPIO.output(3, False)
-	RPi.GPIO.output(4, False)
-	RPi.GPIO.output(17, False)
-	RPi.GPIO.output(27, False)
+	RPi.GPIO.setup(gpioNumber[gpioIndex], RPi.GPIO.OUT)
+
+	RPi.GPIO.output(gpioNumber[gpioIndex], False)
 
 	while True:
 		# turn on outputs for brief time
 		if brightness != 0:
-			RPi.GPIO.output(2, True)
-			RPi.GPIO.output(3, True)
-			RPi.GPIO.output(4, True)
-			RPi.GPIO.output(17, True)
-			RPi.GPIO.output(27, True)
+			RPi.GPIO.output(gpioNumber[gpioIndex], True)
 			time.sleep(brightness/1000)
 
 		# turn off outputs for remaining part of time.
 		if brightness != 25:
-			RPi.GPIO.output(2, False)
-			RPi.GPIO.output(3, False)
-			RPi.GPIO.output(4, False)
-			RPi.GPIO.output(17, False)
-			RPi.GPIO.output(27, False)
+			RPi.GPIO.output(gpioNumber[gpioIndex], False)
 			time.sleep((25-brightness)/1000)
 
-		count = count + 1;
-		if count == 40:
-			count=0
-			#print("worker brightness = %d" % (brightness))
+
 			
 
 try:
 	thread.start_new_thread( setBrightnessThread, () )
 except:
-	print "Error: unable to start thread"
+	print "Error: unable to start setBrightnessThread thread"
 
 
 try:
-	thread.start_new_thread( brightnessWorker, () )
+	thread.start_new_thread( brightnessWorker, ("owl", 0) )
+	thread.start_new_thread( brightnessWorker, ("leftbook", 1) )
+	thread.start_new_thread( brightnessWorker, ("bear", 2) )
+	thread.start_new_thread( brightnessWorker, ("troll", 3) )
+	thread.start_new_thread( brightnessWorker, ("tv spider", 4) )
 except:
 	print "Error: unable to start thread"
 
