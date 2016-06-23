daca9925811cb62b7e4f2f6f5aeaa23875eb705a
Michael Weidner <michael.weidner@motorolasolutions.com>
2016-06-23 00:16:40 -0400

    update with base thread implementation

--- python_multi_gpio.py
+++ python_multi_gpio.py
@@ -1,8 +1,31 @@
 import RPi.GPIO
 import time
+import thread
 
 
-try:
+brightness = 0.0
+
+def setBrightnessThread():
+	global brightness
+	while True:
+		brightness = 1.0
+		#print("brightness=%d" %(brightness))
+		time.sleep(1)
+
+		brightness = 7.0
+		#print("brightness=%d" %(brightness))
+		time.sleep(1)
+	
+		brightness = 24.0
+		#print("brightness=%d" %(brightness))
+		time.sleep(1)
+
+
+
+def brightnessWorker():
+	global brightness
+
+	count = 0
 	RPi.GPIO.setmode(RPi.GPIO.BCM)
 	
 	# pin 11 is GPIO17
@@ -19,37 +42,40 @@ try:
 	RPi.GPIO.output(27, False)
 
 	while True:
+		# turn on outputs for brief time
 		RPi.GPIO.output(2, True)
-		time.sleep(1)
-		RPi.GPIO.output(2, False)
-		time.sleep(1)
-	
 		RPi.GPIO.output(3, True)
-		time.sleep(1)
-		RPi.GPIO.output(3, False)
-		time.sleep(1)
-	
 		RPi.GPIO.output(4, True)
-		time.sleep(1)
-		RPi.GPIO.output(4, False)
-		time.sleep(1)
-	
 		RPi.GPIO.output(17, True)
-		time.sleep(1)
-		RPi.GPIO.output(17, False)
-		time.sleep(1)
-	
 		RPi.GPIO.output(27, True)
-		time.sleep(1)
-		RPi.GPIO.output(27, False)
-		time.sleep(1)
+		time.sleep(brightness/1000)
 
-finally:  
-	RPi.GPIO.setup(2, RPi.GPIO.IN)
-	RPi.GPIO.setup(3, RPi.GPIO.IN)
-	RPi.GPIO.setup(4, RPi.GPIO.IN)
-	RPi.GPIO.setup(17, RPi.GPIO.IN)
-	RPi.GPIO.setup(27, RPi.GPIO.IN)
-	RPi.GPIO.cleanup() # this ensures a clean exit 
+		# turn off outputs for remaining part of time.
+		RPi.GPIO.output(2, False)
+		RPi.GPIO.output(3, False)
+		RPi.GPIO.output(4, False)
+		RPi.GPIO.output(17, False)
+		RPi.GPIO.output(27, False)
+		time.sleep((25-brightness)/1000)
+
+		count = count + 1;
+		if count == 40:
+			count=0
+			#print("worker brightness = %d" % (brightness))
+			
+
+try:
+	thread.start_new_thread( setBrightnessThread, () )
+except:
+	print "Error: unable to start thread"
+
+
+try:
+	thread.start_new_thread( brightnessWorker, () )
+except:
+	print "Error: unable to start thread"
+
+while True:
+	time.sleep(1)
 
 
